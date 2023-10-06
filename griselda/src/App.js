8c90634a8a65d7faf601e12ca48a54c8aabae754
griseldasanchez <gs.griseldasanchez@gmail.com>
2023-10-06 12:38:08 -0700

    Serving react

--- griselda/src/App.js
+++ griselda/src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 const App = () => {
   return (
     <div>
-      <h1>Hello, world! This is served via Express.</h1>
+      <h1>Hello, world! This is served via React.</h1>
     </div>
   );
 };
